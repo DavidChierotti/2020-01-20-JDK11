550860eb067f7d2d8cc176053d9c045cdd8f8a13
DavidChierotti <DavidChierotti@LAPTOP-3C5AULFH>
2022-06-24 12:13:32 +0200

    sol

---
+++ src/main/java/it/polito/tdp/artsmia/model/Artist.java
@@ -0,0 +1,44 @@
+package it.polito.tdp.artsmia.model;
+
+import java.util.Objects;
+
+public class Artist {
+
+	private int id;
+	private String nome;
+	public Artist(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artist other = (Artist) obj;
+		return id == other.id && Objects.equals(nome, other.nome);
+	}
+	
+	
+	
+}
